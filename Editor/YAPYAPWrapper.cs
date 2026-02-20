aec2c82e6ff909d8b55fa246297656a6a8e7d579
ThatDRW <d.renswoude@gmail.com>
2026-02-20 19:10:09 +0100

    1W 261E - GUIDS Assigning, breaks on last compile. Most scripts linked.

--- Editor/YAPYAPWrapper.cs
+++ Editor/YAPYAPWrapper.cs
@@ -9,6 +9,49 @@ public static class YAPYAPWrapper
 {
     public static void GetSteps(StepPipeline stepPipeline)
     {
-        stepPipeline.SetInputSystem(InputSystemType.InputSystem_New);
+        stepPipeline.Steps.Clear();
+        stepPipeline.InsertLast(new GenerateDefaultProjectStructureStep());
+        // stepPipeline.InsertLast(new ImportTextMeshProStep());
+        stepPipeline.InsertLast(new GenerateGitIgnoreStep());
+        // stepPipeline.InsertLast(new GenerateReadmeStep());
+        stepPipeline.InsertLast(new PackagesInstallerStep()); // recompile
+        stepPipeline.InsertLast(new CacheProjectCatalogueStep());
+        stepPipeline.InsertLast(new AssetRipperStep());
+        stepPipeline.InsertLast(new CopyGamePluginsStep()); // recompile // OccaSoftware excluded from DLLs to copy
+
+
+        stepPipeline.InsertLast(new CopyExplicitScriptFolderStep()); // restarts
+        stepPipeline.InsertLast(new EnableUnsafeCodeStep()); // recompiles
+        stepPipeline.InsertLast(new CopyProjectSettingsStep(allowUnsafeCode: true)); // restart
+        
+        stepPipeline.InsertLast(new GuidRemapperStep());
+        stepPipeline.InsertLast(new CopyAssetRipperExportToProjectStep()); // restarts //  PATCHER BREAKING HERE  //
+        stepPipeline.InsertLast(new FixProjectFileIdsStep());
+        stepPipeline.InsertLast(new SortAssetTypesSteps());
+        stepPipeline.InsertLast(new RestartEditorStep());
+
+        // stepPipeline.SetInputSystem(InputSystemType.InputSystem_New);
+
+        // - [0] GenerateDefaultProjectStructureStep
+        // - [1] ImportTextMeshProStep
+        // - [2] GenerateGitIgnoreStep
+        // - [3] GenerateReadmeStep
+        // - [4] PackagesInstallerStep
+        // - [5] EnableNewInputSystemStep
+        // - [6] CacheProjectCatalogueStep
+        // - [7] AssetRipperStep
+        // - [8] CopyGamePluginsStep
+        // - [9] CopyExplicitScriptFolderStep
+        // - [10] EnableUnsafeCodeStep
+        // - [11] CopyProjectSettingsStep
+        // - [12] GuidRemapperStep
+        // - [13] CopyAssetRipperExportToProjectStep
+        // - [14] FixInputSystemAssetsStep
+        // - [15] FixProjectFileIdsStep
+        // - [16] InjectURPAssetsStep
+        // - [17] SortAssetTypesSteps
+        // - [18] RestartEditorStep
+
+
     }
 }
